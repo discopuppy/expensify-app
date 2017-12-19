e474d45a64d991d50d20abe2487f9e3c18223e48
Zachary Moss <zacharymoss@Zacharys-MacBook-Pro.local>
2017-12-19 13:59:44 -0600

    Add Public Route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
@@ -28,6 +27,9 @@ const AppRouter = () => (
     // it with <BrowserRouter>, uses the browser history by default,
     // but with createHistory we are forcing in our own history to manipulate routes
     // <Router> allows us to provide our own history prop.
+
+    // Basically, Private Route and Public Route are like we are passing through a
+    // filter which manipulates the user experience
 );
 
 export default AppRouter;
